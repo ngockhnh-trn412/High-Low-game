61206803e8f216c874482da8182cc6a8a1dfec7c
Khanh Ngoc Tran <trankhanhngoc124@gmail.com>
2026-04-24 21:10:47 +0700

    upload high low game

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+signed main(){
+    srand(static_cast<unsigned int>(time(0)));
+    int secret=rand()%100+1;
+    int usernum=0;
+    int cnt=1;
+    cout<<"Welcome to Ngot's show!!!"<<endl;
+    cout<<"Ngot has chosen a number from 1 to 100, can you guess the chosen number?"<<endl;
+    while(usernum!=secret){
+        cout<<"Guess a number:"<<' ';
+        cin>>usernum;
+        if(usernum<secret){
+            cout<<"Bigger! Please try again"<<' ';
+            cnt++;
+        }
+        else if(usernum>secret){
+            cout<<"Smaller! Please try again"<<' ';
+            cnt++;
+        }
+        else{
+            cout<<"Hooray! You got it in "<<cnt<<" tries!";
+        }
+    }
+ return 0;
+}
